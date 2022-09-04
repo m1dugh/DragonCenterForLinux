9e0958cde25cf9a7718e1e6fca25b2ad3670c32f
m1dugh <romain103paris@gmail.com>
2022-09-04 18:18:08 +0200

    adds env file for registers

---
+++ src/headers/env.h
@@ -0,0 +1,34 @@
+#ifndef DG2_ENV	
+#define DG2_ENV
+
+#define EC_FILE "/sys/kernel/debug/ec/ec0/io"
+
+#define BATTERY_THRESOLD 0xEF
+
+#define CPU_TEMP 0x68
+#define CPU1_TEMP 0x6A
+#define CPU2_TEMP CPU1_TEMP+0x1
+#define CPU3_TEMP CPU1_TEMP+0x2
+#define CPU4_TEMP CPU1_TEMP+0x3
+#define CPU5_TEMP CPU1_TEMP+0x4
+#define CPU6_TEMP CPU1_TEMP+0x5
+
+#define GPU_TEMP 0x80
+#define GPU1_TEMP 0x82
+#define GPU2_TEMP GPU1_TEMP+0x1
+#define GPU3_TEMP GPU1_TEMP+0x2
+#define GPU4_TEMP GPU1_TEMP+0x3
+#define GPU5_TEMP GPU1_TEMP+0x4
+#define GPU6_TEMP GPU1_TEMP+0x5
+
+#define CPU_FAN_RPM 0xCA
+#define GPU_FAN_RPM 0xCC
+
+#define COOLER_BOOST 0x98
+
+#define FAN_MODE 0xF4
+#define FAN_MODE_ADVANCED 0x8C
+#define FAN_MODE_BASIC 0x4C
+#define FAN_MODE_AUTO 0xC
+
+#endif
