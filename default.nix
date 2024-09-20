0604f0ef79fada5c3009b98f613f160843fb33c6
m1dugh <romain.le-miere@epita.fr>
2024-09-20 15:33:31 +0200

    feat(service): moves to hardware.msi.dragon-center

--- default.nix
+++ default.nix
@@ -3,83 +3,84 @@
 , ...
 }:
 let
-    fs = lib.fileset;
-    sourceFiles = fs.difference ./. (fs.unions [
-            ./service.nix
-            ./flake.nix
-            ./flake.lock
-            ./default.nix
-            ./.gitignore
-            ./LICENSE
-    ]);
-    desktop = pkgs.makeDesktopItem {
-        name = "dragon-center";
-        desktopName = "Dragon Center for linux";
-        comment = "MSI Dragon Center for linux";
-        genericName = "Dragon Center for linux";
-        categories = [ "Game" ];
-        exec = "dragon-center-launcher";
-    };
-
-    source = pkgs.rustPlatform.buildRustPackage rec {
-      pname = "dragon-center";
-      version = "0.0.1";
-
-        src = fs.toSource {
-            root = ./.;
-            fileset = sourceFiles;
-        };
-
-      libraryPath = with pkgs; [
-          wayland
-          libxkbcommon
-          fontconfig
-      ];
-
-      cargoLock.lockFile = ./Cargo.lock;
-
-      enableParallelBuilding = true;
-
-      nativeBuildInputs = with pkgs; [
-        pkg-config
-      ] ++ libraryPath;
-
-      postFixup = ''
-        patchelf $out/bin/dragon-center \
-            --set-rpath ${lib.makeLibraryPath libraryPath}
-      '';
-
-      meta.mainProgram = pname;
-    };
-
-    launcher = pkgs.writeShellScriptBin "dragon-center-launcher" ''
-        pkexec env \
-            DISPLAY=$DISPLAY \
-            WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
-            XDG_SESSION_TYPE=$XDG_SESSION_TYPE \
-            XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
-            ${lib.getExe source}
-    '';
-in pkgs.stdenv.mkDerivation {
-    src = source;
-
+  fs = lib.fileset;
+  sourceFiles = fs.difference ./. (fs.unions [
+    ./service.nix
+    ./flake.nix
+    ./flake.lock
+    ./default.nix
+    ./.gitignore
+    ./LICENSE
+  ]);
+  desktop = pkgs.makeDesktopItem {
     name = "dragon-center";
+    desktopName = "Dragon Center for linux";
+    comment = "MSI Dragon Center for linux";
+    genericName = "Dragon Center for linux";
+    categories = [ "Game" ];
+    exec = "dragon-center-launcher";
+  };
 
-    installPhase = ''
-        runHook preInstall
-        mkdir -p $out/bin
-        ln -s $src/bin/dragon-center $out/bin/dragon-center
-        runHook postInstall
-    '';
+  source = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "dragon-center";
+    version = "0.0.1";
 
-    postInstall = ''
-        ln -s ${launcher}/bin/dragon-center-launcher $out/bin/
-        mkdir -p $out/share/applications
-        ln -s ${desktop}/share/applications/* $out/share/applications
-    '';
-
-    meta = {
-        mainProgram = "dragon-center";
-        licences = lib.licences.mit;
+    src = fs.toSource {
+      root = ./.;
+      fileset = sourceFiles;
     };
+
+    libraryPath = with pkgs; [
+      wayland
+      libxkbcommon
+      fontconfig
+    ];
+
+    cargoLock.lockFile = ./Cargo.lock;
+
+    enableParallelBuilding = true;
+
+    nativeBuildInputs = with pkgs; [
+      pkg-config
+    ] ++ libraryPath;
+
+    postFixup = ''
+      patchelf $out/bin/dragon-center \
+          --set-rpath ${lib.makeLibraryPath libraryPath}
+    '';
+
+    meta.mainProgram = pname;
+  };
+
+  launcher = pkgs.writeShellScriptBin "dragon-center-launcher" ''
+    pkexec env \
+        DISPLAY=$DISPLAY \
+        WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+        XDG_SESSION_TYPE=$XDG_SESSION_TYPE \
+        XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+        ${lib.getExe source}
+  '';
+in
+pkgs.stdenv.mkDerivation {
+  src = source;
+
+  name = "dragon-center";
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    ln -s $src/bin/dragon-center $out/bin/dragon-center
+    runHook postInstall
+  '';
+
+  postInstall = ''
+    ln -s ${launcher}/bin/dragon-center-launcher $out/bin/
+    mkdir -p $out/share/applications
+    ln -s ${desktop}/share/applications/* $out/share/applications
+  '';
+
+  meta = {
+    mainProgram = "dragon-center";
+    licences = lib.licences.mit;
+  };
 }
--- service.nix
+++ service.nix
@@ -3,13 +3,30 @@
 , pkgs
 , ...
 }:
-with lib;
 let
+  inherit (lib)
+    mkOption
+    mkEnableOption
+    types
+    mkIf
+    ;
   cfg = config.services.dragon-center;
-  format = pkgs.formats.yaml { };
+
+  driverOptions = {
+    options = {
+      enable = mkEnableOption "MSI ec driver config";
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.msi-ec-kmods;
+        defaultText = "pkgs.msi-ec-kmods";
+        description = "The driver package to use";
+      };
+    };
+  };
 in
 {
-  options.services.dragon-center = {
+  options.hardware.msi.dragon-center = {
     enable = mkEnableOption "Dragon center service";
 
     package = mkOption {
@@ -19,68 +36,27 @@ in
       description = "The package to use";
     };
 
-    withBootOptions = mkOption {
-      default = true;
-      type = types.bool;
-      description = "Whether to add the boot options for embedded controller";
-    };
-
-    layout = mkOption {
-      type = types.nullOr format.type;
-      default = null;
-      description = ''
-        The embedded controller layout to use for this computer.
-      '';
-    };
-
-    layoutFile = mkOption {
-      type = types.nullOr types.path;
-      default = "${cfg.package}/share/config.yaml";
-      description = ''
-        The path to the embedded controller layout to use for this computer
-      '';
+    driver = mkOption {
+      type = driverOptions;
+      description = "The options for the driver";
+      default = { };
     };
   };
 
   config = mkIf cfg.enable {
 
-    assertions = [{
-      assertion = xor (cfg.layoutFile == null) (cfg.layout == null);
-      message = "Only one of `services.dragon-center.layoutFile` and `services.dragon-center.layout` should be set";
-    }];
+    boot = mkIf cfg.driver.enable {
+      extraModulePackages = [
+        cfg.driver.package
+      ];
 
-    boot = mkIf cfg.withBootOptions {
-      kernelModules = [ "ec_sys" ];
-      extraModprobeConfig = ''
-        options ec_sys write_support=1
-      '';
+      kernelPackages = config.boot.kernelPackages // {
+        msi-ec = cfg.driver.package;
+      };
     };
 
     environment.systemPackages = [
       cfg.package
     ];
-
-    systemd.services.dragon-center =
-      let
-        configFile =
-          if cfg.layout != null then format.generate "config.yaml" cfg.layout
-          else cfg.layoutFile;
-      in
-      {
-        wantedBy = [ "multi-user.target" ];
-
-        description = "starts the daemon for dragon center";
-        path = [
-          cfg.package
-        ];
-
-        serviceConfig = {
-          ExecStart = "${getExe cfg.package} --config ${configFile}";
-          Type = "simple";
-          User = "root";
-          RestartSec = "5s";
-          Restart = "on-failure";
-        };
-      };
   };
 }
