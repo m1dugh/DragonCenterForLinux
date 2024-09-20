cdc4573719dfcb0c7eb4ee45a42382638910573a
m1dugh <romain.le-miere@epita.fr>
2024-09-20 16:32:03 +0200

    feat(dragon-center): adds panel for shift and fan mode

--- src/ec.rs
+++ src/ec.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::fs::{self, read_to_string};
 use std::io::{Error, ErrorKind, Read, Result, Write};
 
 
@@ -28,6 +28,47 @@ impl From<&str> for BatteryMode {
     }
 }
 
+pub enum CoolerBoost {
+    On,
+    Off,
+}
+
+impl From<&str> for CoolerBoost {
+    fn from(val: &str) -> CoolerBoost {
+        match val {
+            "on" => CoolerBoost::On,
+            "off" | _ => CoolerBoost::Off,
+        }
+    }
+}
+
+impl From<bool> for CoolerBoost {
+    fn from(val: bool) -> CoolerBoost {
+        match val {
+            true => CoolerBoost::On,
+            false => CoolerBoost::Off,
+        }
+    }
+}
+
+impl Into<bool> for CoolerBoost {
+    fn into(self) -> bool {
+        match self {
+            Self::On => true,
+            Self::Off => false,
+        }
+    }
+}
+
+impl ToString for CoolerBoost {
+    fn to_string(&self) -> String {
+        match self {
+            Self::On => "on",
+            Self::Off => "off",
+        }.to_string()
+    }
+}
+
 pub fn set_battery_mode(battery_mode: BatteryMode) -> Result<()> {
     let mut file = fs::OpenOptions::new()
         .append(true)
@@ -48,4 +89,75 @@ pub fn get_battery_mode() -> Result<BatteryMode> {
         Ok(val) => Ok(BatteryMode::from(val)),
         Err(e) => Err(Error::new(ErrorKind::Other, e.to_string())),
     }
+}
+
+pub fn get_available_shift_modes() -> Result<Vec<String>> {
+    Ok(read_to_string("/sys/devices/platform/msi-ec/available_shift_modes")?
+        .lines()
+        .map(String::from)
+        .collect())
+}
+
+pub fn get_current_shift_mode() -> Result<String> {
+    match read_to_string("/sys/devices/platform/msi-ec/shift_mode")?
+        .lines()
+        .take(1)
+        .map(String::from)
+        .nth(0) {
+            Some(val) => Ok(val),
+            None => Err(Error::new(ErrorKind::Other, "Could not find the current shift mode")),
+        }
+}
+
+pub fn set_shift_mode(shift_mode: String) -> Result<()> {
+    let mut file = fs::OpenOptions::new()
+        .append(true)
+        .open("/sys/devices/platform/msi-ec/shift_mode")?;
+    file.write_fmt(format_args!("{}", shift_mode))?;
+    Ok(())
+}
+
+pub fn get_available_fan_modes() -> Result<Vec<String>> {
+    Ok(read_to_string("/sys/devices/platform/msi-ec/available_fan_modes")?
+        .lines()
+        .map(String::from)
+        .collect())
+}
+
+pub fn get_current_fan_mode() -> Result<String> {
+    match read_to_string("/sys/devices/platform/msi-ec/fan_mode")?
+        .lines()
+        .take(1)
+        .map(String::from)
+        .nth(0) {
+            Some(val) => Ok(val),
+            None => Err(Error::new(ErrorKind::Other, "Could not find the current shift mode")),
+        }
+}
+
+pub fn set_fan_mode(fan_mode: String) -> Result<()> {
+    let mut file = fs::OpenOptions::new()
+        .append(true)
+        .open("/sys/devices/platform/msi-ec/fan_mode")?;
+    file.write_fmt(format_args!("{}", fan_mode))?;
+    Ok(())
+}
+
+pub fn get_cooler_boost() -> Result<CoolerBoost> {
+    match read_to_string("/sys/devices/platform/msi-ec/cooler_boost")?
+        .lines()
+        .take(1)
+        .map(CoolerBoost::from)
+        .nth(0) {
+            Some(val) => Ok(val),
+            None => Err(Error::new(ErrorKind::Other, "Could not find the current shift mode")),
+        }
+}
+
+pub fn set_cooler_boost(cooler_boost: CoolerBoost) -> Result<()> {
+    let mut file = fs::OpenOptions::new()
+        .append(true)
+        .open("/sys/devices/platform/msi-ec/fan_mode")?;
+    file.write_fmt(format_args!("{}", cooler_boost.to_string()))?;
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ mod ec;
 
 use std::error::Error;
 
-use ec::BatteryMode;
+use ec::{BatteryMode, CoolerBoost};
+use slint::{ModelRc, SharedString, VecModel};
 
 slint::include_modules!();
 
@@ -15,6 +16,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     let battery_mode = ec::get_battery_mode()?;
 
     ui.set_battery_mode(battery_mode.to_string().into());
+    ui.set_shift_current_value(ec::get_current_shift_mode()?.into());
+    ui.set_fan_current_value(ec::get_current_fan_mode()?.into());
+
+    let available_shift_modes: Vec::<SharedString> = ec::get_available_shift_modes()?.into_iter().map(Into::into).collect();
+    ui.set_shift_model(ModelRc::new(VecModel::from(available_shift_modes.clone())));
+
+    let available_fan_modes: Vec::<SharedString> = ec::get_available_fan_modes()?.into_iter().map(Into::into).collect();
+    ui.set_fan_model(ModelRc::new(VecModel::from(available_fan_modes.clone())));
+
+    ui.set_cooler_boost_value(ec::get_cooler_boost()?.into());
 
     ui.on_request_battery_update(|battery| {
         let battery_mode = BatteryMode::from(battery.to_string().as_str());
@@ -22,6 +33,33 @@ fn main() -> Result<(), Box<dyn Error>> {
         match ec::set_battery_mode(battery_mode) {
             Err(e) => eprintln!("{}", e),
             _ => {}
+        }
+    });
+
+    ui.on_request_shift_update(|shift| {
+        match ec::set_shift_mode(shift.to_string()) {
+            Err(e) => eprintln!("{}", e),
+            _ => {}
+        }
+    });
+
+
+    ui.on_request_fan_update(|fan| {
+        match ec::set_fan_mode(fan.to_string()) {
+            Err(e) => eprintln!("{}", e),
+            _ => {}
+        }
+    });
+
+    ui.on_request_cooler_boost_update({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            let cooler_boost = CoolerBoost::from(ui.get_cooler_boost_value());
+            match ec::set_cooler_boost(cooler_boost) {
+                Err(e) => eprintln!("{}", e),
+                _ => {}
+            };
         }
     });
 
